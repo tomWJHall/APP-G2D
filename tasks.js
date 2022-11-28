9753a8b3a62bc9c9856bf24569c5cca0d0837b63
tomWJHall <tomhpar@gmail.com>
2022-11-28 01:20:28 +0100

    Trying to create a function to access the raw text file from the web.

--- tasks.js
+++ tasks.js
@@ -6,20 +6,28 @@ const Tasks = () => {
   function readTextFile(file) {
     
     var rawFile = new XMLHttpRequest();
+    rawFile.onreadystatechange = (e) => {
+      if (rawFile.readyState !== 4) {
+        return;
+      }
+    
+      if (request.status === 200) {
+        console.log('success', rawFile.responseText);
+        var allText = rawFile.responseText;
+      } else {
+        console.warn('error');
+      }
+    };
+    
     rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                alert(allText);
-            }
-        }
-    }
-    rawFile.send(null);
+    rawFile.send();
+
+    return allText;
   }
+
+  var taskContent = readTextFile("https://raw.githubusercontent.com/tomWJHall/APP-G2D/master/tasks.txt");
+
+  setInterval(() => {console.log(taskContent)}, 1000);
 
     return(
       <View style={styles.container}>
